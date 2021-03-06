780245b03ea9bd7fb398284e763c47411fad79d5
clannadxr <clannadxr@hotmail.com>
2021-03-06 22:15:05 +0800

    升级 resty 以支持 `OnError` hook 并记录 error 日志

--- client/ehttp/component.go
+++ client/ehttp/component.go
@@ -21,39 +21,78 @@ type Component struct {
 }
 
 func newComponent(name string, config *Config, logger *elog.Component) *Component {
-	restyClient := resty.New().SetDebug(config.RawDebug).SetTimeout(config.ReadTimeout).SetHeader("app", eapp.Name()).OnAfterResponse(func(client *resty.Client, response *resty.Response) error {
-		rr := response.Request.RawRequest
+	var logAccess = func(request *resty.Request, response *resty.Response, err error) {
+		rr := request.RawRequest
+		fullMethod := request.Method + "." + rr.URL.RequestURI() // GET./hello
+		var (
+			cost     time.Duration
+			respBody string
+		)
+		if response != nil {
+			cost = response.Time()
+			respBody = string(response.Body())
+		}
 		if eapp.IsDevelopmentMode() {
-			log.Println("http.response", xdebug.MakeReqResInfo(name, config.Addr, response.Time(), response.Request.Method+"."+rr.URL.RequestURI(), string(response.Body())))
+			if err != nil {
+				log.Println("http.response", xdebug.MakeReqResError(name, config.Addr, cost, fullMethod, err.Error()))
+			} else {
+				log.Println("http.response", xdebug.MakeReqResInfo(name, config.Addr, cost, fullMethod, respBody))
+			}
 		}
 
-		isSlowLog := false
-		// todo can't log error
+		var isSlowLog, isErrLog bool
 		var fields = make([]elog.Field, 0, 15)
 
 		fields = append(fields,
-			elog.FieldMethod(response.Request.Method+"."+rr.URL.RequestURI()), // GET./hello
+			elog.FieldMethod(fullMethod),
 			elog.FieldName(name),
-			elog.FieldCost(response.Time()),
+			elog.FieldCost(cost),
 			elog.FieldAddr(rr.URL.Host),
 		)
 
 		if config.EnableAccessInterceptorReply {
-			fields = append(fields, elog.FieldValueAny(string(response.Body())))
+			fields = append(fields, elog.FieldValueAny(respBody))
 		}
 
-		if config.SlowLogThreshold > time.Duration(0) && response.Time() > config.SlowLogThreshold {
+		if err != nil {
+			elog.FieldErr(err)
+			if response == nil {
+				// 无 response 的是连接超时等系统级错误
+				fields = append(fields, elog.FieldEvent("error"))
+				logger.Error("access", fields...)
+			} else {
+				logger.Warn("access", fields...)
+			}
+			isErrLog = true
+		}
+
+		if config.SlowLogThreshold > time.Duration(0) && cost > config.SlowLogThreshold {
 			fields = append(fields, elog.FieldEvent("slow"))
 			logger.Warn("access", fields...)
 			isSlowLog = true
 		}
 
-		if config.EnableAccessInterceptor && !isSlowLog {
+		if config.EnableAccessInterceptor && !isSlowLog && !isErrLog {
 			fields = append(fields, elog.FieldEvent("normal"))
 			logger.Info("access", fields...)
 		}
-		return nil
-	}).SetHostURL(config.Addr)
+	}
+	restyClient := resty.New().
+		SetDebug(config.RawDebug).
+		SetTimeout(config.ReadTimeout).
+		SetHeader("app", eapp.Name()).
+		OnAfterResponse(func(client *resty.Client, response *resty.Response) error {
+			logAccess(response.Request, response, nil)
+			return nil
+		}).
+		OnError(func(req *resty.Request, err error) {
+			if v, ok := err.(*resty.ResponseError); ok {
+				logAccess(req, v.Response, v.Err)
+			} else {
+				logAccess(req, nil, err)
+			}
+		}).
+		SetHostURL(config.Addr)
 	return &Component{
 		name:   name,
 		config: config,
--- go.mod
+++ go.mod
@@ -6,9 +6,10 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/HdrHistogram/hdrhistogram-go v1.0.1 // indirect
 	github.com/davecgh/go-spew v1.1.1
+	github.com/frankban/quicktest v1.11.3 // indirect
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-resty/resty/v2 v2.3.0
+	github.com/go-resty/resty/v2 v2.5.0
 	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/websocket v1.4.2
 	github.com/json-iterator/go v1.1.10
@@ -24,7 +25,7 @@ require (
 	github.com/uber/jaeger-lib v2.4.0+incompatible // indirect
 	go.uber.org/automaxprocs v1.3.0
 	go.uber.org/zap v1.15.0
-	golang.org/x/net v0.0.0-20201006153459-a7d1128ccaa0
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 	google.golang.org/genproto v0.0.0-20200806141610-86f49bd18e98
 	google.golang.org/grpc v1.29.1
