6f45c266d36b48832c913b2aaca06c55f247a0e4
sevennt <sevennt.leslie@gmail.com>
2021-06-20 23:34:59 +0800

    fix test failure

--- server/egin/interceptor_test.go
+++ server/egin/interceptor_test.go
@@ -11,8 +11,9 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gotomicro/ego/core/elog"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/gotomicro/ego/core/elog"
 )
 
 func TestPanicInHandler(t *testing.T) {
@@ -32,10 +33,10 @@ func TestPanicInHandler(t *testing.T) {
 	assert.Nil(t, err)
 	// TEST
 	assert.Equal(t, http.StatusInternalServerError, w.Code)
-	assert.Contains(t, string(logged), `"event":"recover"`)
+	assert.Contains(t, string(logged), `"event": "recover"`)
 	assert.Contains(t, string(logged), "we have a panic")
 	assert.Contains(t, string(logged), t.Name())
-	assert.Contains(t, string(logged), `"type":"GET","method":"/recovery"`)
+	assert.Contains(t, string(logged), `"method": "GET./recovery"`)
 	os.Remove(path.Join(logger.GetConfigDir(), logger.GetConfigName()))
 }
 
