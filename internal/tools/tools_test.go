5e5f878fa6d7f7f71c522e12bbf6fbef1893f7a8
askuy <askuygo@foxmail.com>
2021-07-29 09:50:01 +0800

    add unit test

--- internal/tools/tools_test.go
+++ internal/tools/tools_test.go
@@ -18,6 +18,11 @@ func TestGrpcHeaderValue(t *testing.T) {
 	assert.Equal(t, "9527", value)
 }
 
+func TestGrpcHeaderValueEmpty(t *testing.T) {
+	value := GrpcHeaderValue(context.Background(), "X-Ego-Uid")
+	assert.Equal(t, "", value)
+}
+
 func TestContextValue(t *testing.T) {
 	transport.Set([]string{"X-Ego-Uid"})
 
@@ -28,4 +33,9 @@ func TestContextValue(t *testing.T) {
 	ctx = transport.WithValue(context.Background(), "X-Ego-Uid", 9528)
 	value = ContextValue(ctx, "X-Ego-Uid")
 	assert.Equal(t, "9528", value)
+}
+
+func TestContextValueEmpty(t *testing.T) {
+	value := ContextValue(context.Background(), "X-Ego-Uid")
+	assert.Equal(t, "", value)
 }
