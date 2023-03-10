7323f4b910c91cd3e2cad796a54d48afd5eb3a83
askuy <askuygo@foxmail.com>
2023-03-10 21:22:09 +0800

    support unix socket

---
+++ examples/grpc/unix/server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+
+	"github.com/gotomicro/ego/server/egovernor"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/gotomicro/ego"
+	"github.com/gotomicro/ego/core/elog"
+	"github.com/gotomicro/ego/examples/helloworld"
+	"github.com/gotomicro/ego/server"
+	"github.com/gotomicro/ego/server/egrpc"
+)
+
+// export EGO_DEBUG=true && go run main.go --config=config.toml
+func main() {
+	if err := ego.New().Serve(func() server.Server {
+		component := egrpc.Load("server.grpc").Build()
+		helloworld.RegisterGreeterServer(component.Server, &Greeter{server: component})
+		return component
+	}(), egovernor.Load("server.governor").Build()).Run(); err != nil {
+		elog.Panic("startup", elog.FieldErr(err))
+	}
+}
+
+// Greeter ...
+type Greeter struct {
+	server *egrpc.Component
+	helloworld.UnimplementedGreeterServer
+}
+
+// SayHello ...
+func (g Greeter) SayHello(ctx context.Context, request *helloworld.HelloRequest) (*helloworld.HelloResponse, error) {
+	if request.Name == "error" {
+		return nil, status.Error(codes.Unavailable, "error")
+	}
+
+	// time.Sleep(xtime.Duration("2s"))
+	return &helloworld.HelloResponse{
+		Message: "Hello EGO, I'm222 " + g.server.Address(),
+	}, nil
+}
