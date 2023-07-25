57080cc243d677c1a00a83dd14e028ed6f589bbb
Jarily <11690345@qq.com>
2023-07-25 10:57:47 +0800

    governor host modifiy optimize

--- server/egovernor/container.go
+++ server/egovernor/container.go
@@ -32,6 +32,8 @@ func Load(key string) *Container {
 		return c
 	}
 	// 修改host信息
+	// governor的host如果是127.0.0.1或者0.0.0.0的话 prometheus是无法拉取到metric信息的
+	// eflag.String("host")的默认值就是0.0.0.0 所以这里需要判断一下 不能无脑的修改
 	host := eflag.String("host")
 	if host != "" && host != "127.0.0.1" && host != "0.0.0.0" {
 		c.config.Host = host
